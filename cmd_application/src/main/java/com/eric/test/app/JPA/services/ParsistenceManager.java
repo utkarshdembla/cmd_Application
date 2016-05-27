96128eb83c9fdc5eed4d14bb1b183df4f1d79881
Dembla Utkarsh <dembla.utkarsh@ericsson.com>
2016-05-27 13:15:48 +0530

    new update

---
+++ cmd_application/src/main/java/com/eric/test/app/JPA/services/ParsistenceManager.java
@@ -0,0 +1,20 @@
+package com.eric.test.app.JPA.services;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+public enum ParsistenceManager {
+  INSTANCE;
+  private EntityManagerFactory emFactory;
+  private ParsistenceManager() {
+    // "jpa-example" was the value of the name attribute of the
+    // persistence-unit element.
+    emFactory = Persistence.createEntityManagerFactory("cmd");
+  }
+  public EntityManager getEntityManager() {
+    return emFactory.createEntityManager();
+  }
+  public void close() {
+    emFactory.close();
+  }
+}
